520044abd1b9c6148511382a4dec2a170cb4e31f
Zhekai <limzhekai1999@live.com>
2024-09-04 22:35:05 +0800

    Restructure test related files

--- src/features/Home/components/__tests__/Welcome.test.tsx
+++ src/features/Home/components/__tests__/Welcome.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@test-utils";
+import { render, screen } from "@test/testUtils";
 import { Welcome } from "../Welcome";
 
 describe("Welcome component", () => {
--- src/test/render.tsx
+++ src/test/render.tsx
@@ -1,7 +1,7 @@
 import { MantineProvider } from "@mantine/core";
 import { render as testingLibraryRender } from "@testing-library/react";
 import React from "react";
-import { theme } from "../src/app/theme";
+import { theme } from "../app/theme";
 
 export function render(ui: React.ReactNode) {
   return testingLibraryRender(ui, {
