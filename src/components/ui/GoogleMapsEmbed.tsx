46bb315221a5beb69bd9c5c5e24489567ee394be
Zhekai <limzhekai1999@live.com>
2024-09-07 21:02:57 +0800

    Implement PlaceCard and GoogleMapsEmbed

---
+++ src/components/ui/GoogleMapsEmbed.tsx
@@ -0,0 +1,38 @@
+import clsx from "clsx";
+import React, { useMemo } from "react";
+
+type Props = {
+  placeId: string;
+  style?: React.CSSProperties | null;
+  className?: clsx.ClassValue;
+};
+
+export const GoogleMapsEmbed: React.FC<Props> = ({
+  placeId,
+  className,
+  style,
+}) => {
+  const computedStyle = useMemo(() => {
+    if (style === null) {
+      return {};
+    }
+    const { border, width, height, ...rest } = style ?? {};
+    return {
+      border: border ?? 0,
+      width: width ?? "100%",
+      height: height ?? "100%",
+      ...rest,
+    };
+  }, [style]);
+  return (
+    <iframe
+      className={clsx(className)}
+      style={computedStyle}
+      loading="lazy"
+      allowFullScreen
+      referrerPolicy="no-referrer-when-downgrade"
+      src={`https://www.google.com/maps/embed/v1/place?key=${import.meta.env.VITE_MAPS_API_KEY}
+    &q=place_id:${placeId}`}
+    ></iframe>
+  );
+};
---
+++ src/features/Consumer/components/PlaceCard.tsx
@@ -0,0 +1,57 @@
+import { Place } from "@/types";
+import { Card, Image, Space, Text } from "@mantine/core";
+import React from "react";
+import classes from "./Places.module.scss";
+
+type Props = {
+  place: Place;
+};
+
+const PartialStar: React.FC<{ fill: number }> = ({ fill }) => {
+  const fillPercentage = Math.min(fill * 100, 100);
+  return (
+    <span
+      className={classes.PlaceOverview__Star}
+      style={{
+        backgroundImage: `linear-gradient(
+          90deg,
+          var(--mantine-color-yellow-7),
+          var(--mantine-color-yellow-7) ${fillPercentage}%,
+          var(--mantine-color-dimmed) ${fillPercentage}%
+        )`,
+      }}
+    >
+      ★
+    </span>
+  );
+};
+
+export const PlaceCard: React.FC<Props> = ({ place }) => {
+  return (
+    <Card radius="md" shadow="xs" padding="lg" withBorder>
+      {place.imageUrl && (
+        <Card.Section>
+          <Image src={place.imageUrl} alt={place.name} mah={300} mih={200} />
+        </Card.Section>
+      )}
+      <Space h="sm" />
+      <Text size="xl" fw="bold">
+        {place.name}
+      </Text>
+      <Text size="sm" c="dimmed">
+        {place.weightedRating.toFixed(1) != place.rating.toFixed(1) && (
+          <s>{place.rating.toFixed(1)}</s>
+        )}{" "}
+        {place.weightedRating.toFixed(1)}{" "}
+        {Array.from({ length: Math.ceil(place.weightedRating) }).map((_, i) => (
+          <PartialStar fill={place.weightedRating - i} key={i} />
+        ))}{" "}
+        ({place.userRatingCount})
+      </Text>
+      <Text size="sm" c="dimmed">
+        {place.primaryType}
+      </Text>
+      <Text mt="md">{place.summary}</Text>
+    </Card>
+  );
+};
