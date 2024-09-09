ad66fb3a863e317b0487e8a802ab5b085b4e50aa
Zhekai <limzhekai1999@live.com>
2024-09-09 22:33:53 +0800

    Add test for PlaceCard

---
+++ src/features/Consumer/components/__tests__/PlaceCard.test.tsx
@@ -0,0 +1,67 @@
+import { render } from "@/test/testUtils";
+import { Place } from "@/types/data";
+import { screen } from "@testing-library/react";
+import { format } from "date-fns";
+import { PlaceCard } from "../PlaceCard";
+
+describe("PlaceCard component", () => {
+  const place: Place = {
+    id: "mockId",
+    name: "Sample Place",
+    imageUrl: "https://example.com/image.jpg",
+    weightedRating: 4.5,
+    rating: 4.0,
+    userRatingCount: 100,
+    primaryType: "Restaurant",
+    lastUpdated: new Date().toISOString(),
+    summary: "This is a sample place summary.",
+    location: {
+      lat: 0,
+      lng: 0,
+    },
+  };
+
+  it("renders place name", () => {
+    render(<PlaceCard place={place} />);
+    expect(screen.getByText(place.name)).toBeInTheDocument();
+  });
+
+  it("renders place image if imageUrl is provided", () => {
+    render(<PlaceCard place={place} />);
+    const image = screen.getByAltText(place.name);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute("src", place.imageUrl);
+  });
+
+  it("renders place rating and weighted rating", () => {
+    render(<PlaceCard place={place} />);
+    expect(
+      screen.getByText((content) => {
+        return content.includes(place.weightedRating.toFixed(1));
+      }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText((content) =>
+        content.includes(`(${place.userRatingCount})`),
+      ),
+    ).toBeInTheDocument();
+  });
+
+  it("renders place primary type", () => {
+    render(<PlaceCard place={place} />);
+    expect(screen.getByText(place.primaryType)).toBeInTheDocument();
+  });
+
+  it("renders last updated date", () => {
+    render(<PlaceCard place={place} />);
+    const formattedDate = format(place.lastUpdated, "d MMM yyyy, hh:mm a");
+    expect(
+      screen.getByText(`Last updated: ${formattedDate}`),
+    ).toBeInTheDocument();
+  });
+
+  it("renders place summary", () => {
+    render(<PlaceCard place={place} />);
+    expect(screen.getByText(place.summary)).toBeInTheDocument();
+  });
+});
