9cceb91dacb3278f25b7a7b45b919cc98ea30ac4
Zhekai <limzhekai1999@live.com>
2024-09-19 00:18:27 +0800

    Update HomePage to use `useViewMode` hook

--- src/app/pages/Home.page.tsx
+++ src/app/pages/Home.page.tsx
@@ -1,22 +1,20 @@
 import { FeaturesSection } from "@/features/Home/components/FeaturesSection";
-import {
-  FeatureView,
-  FeatureViewButtonGroup,
-} from "@/features/Home/components/FeatureViewButtonGroup";
+import { FeatureViewButtonGroup } from "@/features/Home/components/FeatureViewButtonGroup";
 import { WelcomeTitle } from "@/features/Home/components/WelcomeTitle";
+import { useViewMode } from "@/hooks/useViewMode";
 import { Container, Space } from "@mantine/core";
-import React, { useState } from "react";
+import React from "react";
 
 const HomePage: React.FC = () => {
-  const [view, setView] = useState<FeatureView>("consumer");
+  const { mode, setMode } = useViewMode();
 
   return (
     <Container>
       <WelcomeTitle />
       <Space h="xl" />
-      <FeatureViewButtonGroup value={view} setter={setView} />
+      <FeatureViewButtonGroup value={mode} setter={setMode} />
       <Space h="xl" />
-      <FeaturesSection view={view} />
+      <FeaturesSection view={mode} />
     </Container>
   );
 };
