4b999889f61721ec8eb43234f176cda3373d99f7
Zhekai <limzhekai1999@live.com>
2024-09-19 20:34:59 +0800

    Remove `ViewModeControl` from `HomePage`

--- src/app/pages/Home.page.tsx
+++ src/app/pages/Home.page.tsx
@@ -1,18 +1,12 @@
 import { FeaturesSection } from "@/features/Home/components/FeaturesSection";
-import { ViewModeControl } from "@/features/Home/components/ViewModeControl";
 import { WelcomeTitle } from "@/features/Home/components/WelcomeTitle";
-import { useViewMode } from "@/hooks/useViewMode";
 import { Container, Space } from "@mantine/core";
 import React from "react";
 
 const HomePage: React.FC = () => {
-  const { viewMode, setViewMode } = useViewMode();
-
   return (
     <Container>
       <WelcomeTitle />
-      <Space h="xl" />
-      <ViewModeControl />
       <Space h="xl" />
       <FeaturesSection />
     </Container>
