7d2f3af9eadf1af5899ea2bf9268b83401a66faf
Zhekai <limzhekai1999@live.com>
2024-09-23 19:24:13 +0800

    Update PlaceCard to add tabs with gallery and summary

--- src/features/Dashboard/components/PlaceCard.tsx
+++ src/features/Dashboard/components/PlaceCard.tsx
@@ -1,15 +1,31 @@
-import { Card, Divider, Image, Space, Text } from "@mantine/core";
+import {
+  Card,
+  Image,
+  ScrollArea,
+  SimpleGrid,
+  Space,
+  Tabs,
+  Text,
+} from "@mantine/core";
 import { format } from "date-fns";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { PartialStar } from "@/components/ui/PartialStar";
 import { Place } from "@/types/data";
+
+import { getMockPlaceImages } from "../api/getPlaceImages";
 
 type Props = {
   place: Place;
 };
 
 export const PlaceCard: React.FC<Props> = ({ place }) => {
+  const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    getMockPlaceImages(place.id).then(setImages);
+  }, [place.id]);
+
   return (
     <Card radius="md" padding="lg" withBorder>
       {place.imageUrl && (
@@ -22,10 +38,7 @@ export const PlaceCard: React.FC<Props> = ({ place }) => {
         {place.name}
       </Text>
       <Text size="sm" c="dimmed">
-        {place.weightedRating.toFixed(1) != place.rating.toFixed(1) && (
-          <s>{place.rating.toFixed(1)}</s>
-        )}{" "}
-        {place.weightedRating.toFixed(1)}{" "}
+        {place.rating.toFixed(1)}{" "}
         {Array.from({ length: Math.ceil(place.weightedRating) }).map((_, i) => (
           <PartialStar fill={place.weightedRating - i} key={i} />
         ))}{" "}
@@ -37,10 +50,37 @@ export const PlaceCard: React.FC<Props> = ({ place }) => {
       <Text size="sm" c="dimmed">
         Last updated: {format(place.lastUpdated, "d MMM yyyy, hh:mm a")}
       </Text>
-      <Card.Section>
-        <Divider my="sm" />
+
+      <Card.Section mt="xs">
+        <Tabs defaultValue="Summary">
+          <Tabs.List>
+            <Tabs.Tab value="Summary">Summary</Tabs.Tab>
+            <Tabs.Tab value="Gallery">Gallery</Tabs.Tab>
+          </Tabs.List>
+
+          <Tabs.Panel value="Summary" p="md">
+            <Text size="sm">{place.summary}</Text>
+          </Tabs.Panel>
+
+          <Tabs.Panel value="Gallery" p="md">
+            <ScrollArea type="always" h={300}>
+              <SimpleGrid cols={3}>
+                {images.map((image) => (
+                  <Image
+                    h="100%"
+                    w="100%"
+                    src={image}
+                    alt={place.name}
+                    key={image}
+                    referrerPolicy="no-referrer"
+                    fit="cover"
+                  />
+                ))}
+              </SimpleGrid>
+            </ScrollArea>
+          </Tabs.Panel>
+        </Tabs>
       </Card.Section>
-      <Text size="sm">{place.summary}</Text>
     </Card>
   );
 };
