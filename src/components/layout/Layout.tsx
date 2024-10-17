d80a7379abd49001408fc372009edbb587047ece
Zhekai <limzhekai1999@live.com>
2024-10-17 21:20:53 +0800

    Improve responsiveness for Header

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,10 +2,15 @@ import {
   ActionIcon,
   Anchor,
   AppShell,
+  Burger,
+  Button,
   MantineColorScheme,
   Space,
+  Stack,
   useMantineColorScheme,
+  useMatches,
 } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { IconDeviceDesktop, IconMoon, IconSun } from "@tabler/icons-react";
 import { Link, matchPath, Outlet, useLocation } from "react-router-dom";
 
@@ -14,23 +19,36 @@ import { ViewModeControl } from "@/features/Home/components/ViewModeControl";
 
 import classes from "./Layout.module.scss";
 
-const NavLink: React.FC<{ path: string; label: string }> = ({
-  path,
-  label,
-}) => {
+const NavLink: React.FC<{
+  path: string;
+  label: string;
+  variant?: "button" | "link";
+  toggleMenu?: () => void;
+}> = ({ path, label, variant = "link", toggleMenu }) => {
   const { pathname } = useLocation();
-  return (
-    <Anchor
-      component={Link}
-      to={path}
-      underline={matchPath(path, pathname) ? "always" : "hover"}
-    >
+  const isMatch = matchPath(path, pathname);
+  return variant == "link" ? (
+    <Anchor component={Link} to={path} underline={isMatch ? "always" : "hover"}>
       <b>{label}</b>
     </Anchor>
+  ) : (
+    <Button
+      onClick={toggleMenu}
+      variant={isMatch ? "filled" : "subtle"}
+      component={Link}
+      to={path}
+    >
+      {label}
+    </Button>
   );
 };
 
-const NavBar: React.FC = () => {
+type HeaderProps = {
+  isMenuOpen: boolean;
+  toggle: () => void;
+};
+
+const AppHeader: React.FC<HeaderProps> = ({ isMenuOpen, toggle }) => {
   const { colorScheme, setColorScheme } = useMantineColorScheme();
   const toggleColorScheme = () => {
     const schemes: MantineColorScheme[] = ["light", "dark", "auto"];
@@ -38,21 +56,32 @@ const NavBar: React.FC = () => {
     const next = schemes[(currentIndex + 1) % schemes.length];
     setColorScheme(next);
   };
+  const isMobile = useMatches({ base: true, xs: false });
+
   return (
     <AppShell.Header className={classes.Layout__AppBar}>
+      {isMobile && <Burger opened={isMenuOpen} onClick={toggle} />}
       <Link to="/">
         <img src={logo} alt="GoodMeh Logo" height={50} />
       </Link>
-      <Space w="md" />
+      {!isMobile && (
+        <>
+          <Space w="md" />
 
-      <NavLink path="/discover" label="Discover" />
-      <Space w="md" />
-      <NavLink path="/compare" label="Compare" />
+          <NavLink path="/discover" label="Discover" />
+          <Space w="md" />
+          <NavLink path="/compare" label="Compare" />
+        </>
+      )}
 
       <Space flex={1} />
 
-      <ViewModeControl />
-      <Space w="md" />
+      {!isMobile && (
+        <>
+          <ViewModeControl />
+          <Space w="md" />
+        </>
+      )}
       <ActionIcon onClick={toggleColorScheme}>
         {colorScheme == "light" ? (
           <IconSun />
@@ -66,13 +95,51 @@ const NavBar: React.FC = () => {
   );
 };
 
+const AppNavbar: React.FC<{ toggleMenu: () => void }> = ({ toggleMenu }) => {
+  return (
+    <AppShell.Navbar p="md">
+      <Stack justify="space-between" h="100%">
+        <Stack>
+          <NavLink
+            path="/"
+            label="Home"
+            variant="button"
+            toggleMenu={toggleMenu}
+          />
+          <NavLink
+            path="/discover"
+            label="Discover"
+            variant="button"
+            toggleMenu={toggleMenu}
+          />
+          <NavLink
+            path="/compare"
+            label="Compare"
+            variant="button"
+            toggleMenu={toggleMenu}
+          />
+        </Stack>
+        <ViewModeControl />
+      </Stack>
+    </AppShell.Navbar>
+  );
+};
+
 export const Layout: React.FC = () => {
+  const [isMenuOpen, { toggle }] = useDisclosure();
+
   return (
     <AppShell
       header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "xs",
+        collapsed: { desktop: true, mobile: !isMenuOpen },
+      }}
       padding={{ base: "md", xs: "lg", sm: "xl" }}
     >
-      <NavBar />
+      <AppHeader isMenuOpen={isMenuOpen} toggle={toggle} />
+      <AppNavbar toggleMenu={toggle} />
       <AppShell.Main>
         <Outlet />
       </AppShell.Main>
