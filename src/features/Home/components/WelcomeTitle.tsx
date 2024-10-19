4fbd8c80d6758f87b694c03a7a3850e79dde23b7
Zhekai <40530851+zhekaiii@users.noreply.github.com>
2024-10-19 20:35:02 +0800

    Mobile Responsiveness for Header and Home page (#67)
    * Make viewMode persist in localStorage
    
    * Update icon for auto colorScheme
    
    * Improve responsivness for Home page
    
    * Improve responsiveness for Header

--- src/features/Home/components/WelcomeTitle.tsx
+++ src/features/Home/components/WelcomeTitle.tsx
@@ -1,18 +1,24 @@
-import { Title } from "@mantine/core";
+import { Text, Title } from "@mantine/core";
 
 import classes from "./WelcomeTitle.module.scss";
 
 export const WelcomeTitle: React.FC = () => {
   return (
     <>
-      <Title className={classes.title} ta="center">
+      <Title className={classes.Welcome__Title} ta="center">
         {"GoodMeh?".split("").map((char, index) => (
           <span key={index}>{char}</span>
         ))}
       </Title>
-      <Title order={6} size="h1" ta="center" maw={580} mx="auto" fw="500">
+      <Text
+        className={classes.Welcome__Subtitle}
+        ta="center"
+        maw={580}
+        mx="auto"
+        fw="500"
+      >
         Good or Meh? You decide lor.
-      </Title>
+      </Text>
     </>
   );
 };
