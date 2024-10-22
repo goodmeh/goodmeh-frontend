bf03dac43fc97f21639dfa78328c96ab2197a5be
Zhekai <limzhekai1999@live.com>
2024-10-22 18:15:39 +0800

    Rename HomePage to LandingPage

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,14 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { Layout } from "@/components/layout/Layout";
 
-const HomePage = () => import("./pages/Home.page");
+const LandingPage = () => import("./pages/Landing.page");
 const DiscoverPage = () => import("./pages/Discover.page");
 
 const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
-      { path: "/", lazy: HomePage },
+      { path: "/", lazy: LandingPage },
       { path: "/discover", lazy: DiscoverPage },
     ],
   },
