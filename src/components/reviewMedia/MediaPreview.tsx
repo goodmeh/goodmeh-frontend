b613c1015b3b9fa92fb101abfdd85a5e531b3fa6
Zhekai <40530851+zhekaiii@users.noreply.github.com>
2024-10-25 15:22:42 +0800

    Review UI Revamp (#77)
    * Add aspect ratio to place card image
    
    * Update ReviewDetails UI
    
    * Add MediaModal to ReviewGallery
    
    * Update copy of Summarised by AI for business
    
    * Improve responsiveness of SearchScreen
    
    * Update MediaPreview and ReviewGallery
    
    * Reorganize review summary button
    
    * Wrap Search and Compare in Container
    
    * Fix review text not showing up
    
    * Convert review helpfulness progress bar to pips

--- src/components/reviewMedia/MediaPreview.tsx
+++ src/components/reviewMedia/MediaPreview.tsx
@@ -1,21 +1,52 @@
-import { Image } from "@mantine/core";
+import { Center, Image, Overlay } from "@mantine/core";
+import { IconPlayerPlay } from "@tabler/icons-react";
 
 type Props = {
   mediaUrl: string;
   height?: number | string;
   width?: number | string;
+  onClick?: () => void;
+  showVideoControls?: boolean;
 };
 
 export const MediaPreview: React.FC<Props> = ({
   mediaUrl,
   height = 400,
   width = 250,
+  onClick,
+  showVideoControls = false,
 }) => {
   const isVideo = mediaUrl.startsWith("https://lh3.googleusercontent.com/ggms");
+  const cursor = onClick ? "pointer" : "default";
 
   if (isVideo) {
-    return <video src={mediaUrl} controls style={{ height, width }} muted />;
+    return (
+      <div style={{ position: "relative", cursor }} onClick={onClick}>
+        {!showVideoControls && (
+          <Overlay backgroundOpacity={0}>
+            <Center h="100%">
+              <IconPlayerPlay color="white" />
+            </Center>
+          </Overlay>
+        )}
+        <video
+          src={mediaUrl}
+          controls={showVideoControls}
+          style={{ height, width }}
+          muted
+        />
+      </div>
+    );
   }
 
-  return <Image loading="lazy" h={height} maw={width} src={mediaUrl} />;
+  return (
+    <Image
+      loading="lazy"
+      h={height}
+      maw={width}
+      src={mediaUrl}
+      onClick={onClick}
+      style={{ cursor }}
+    />
+  );
 };
