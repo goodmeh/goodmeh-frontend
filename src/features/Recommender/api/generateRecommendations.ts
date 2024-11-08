702feba593dee1e3914463321f54dbfc16e7cf77
Zhekai <limzhekai1999@live.com>
2024-11-08 22:10:56 +0800

    Generate multiple recommendations

--- src/features/Recommender/api/generateRecommendations.ts
+++ src/features/Recommender/api/generateRecommendations.ts
@@ -5,11 +5,11 @@ import { Place } from "@/types/data";
 export const generateMockRecommendation = (placeIds: string[]) =>
   discoverPlaces().then((places) => places[0]);
 
-export const generateRecommendation = async (placeIds: string[]) => {
+export const generateRecommendations = async (placeIds: string[]) => {
   const params = new URLSearchParams();
   placeIds.forEach((placeId, index) => {
     params.set(`place_id_${index + 1}`, placeId);
   });
   const response = await api.get<Place[]>("/v1/recommend/", { params });
-  return response.data[0];
+  return response.data;
 };
